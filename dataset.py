more things added to plate ad has some problems fixed a modi calculation bug

dataset.py
@@ -285,9 +285,28 @@ class QSARDataset:
 
         #check for initial missing labels
 
-        missing_labels = np.isnan(self.dataset[self._label_col].astype(float))
-        new_fail_dict = {x:"Missing initial label" for x in self.dataset[missing_labels].index}
-        self._failed.update(new_fail_dict)
+        #TODO vectorize
+        for i, label in enumerate(self.dataset[self._label_col]):
+            try:
+                x = float(label)
+            except:
+                self._failed[i] = "Missing initial label"
+                self.dataset[self._label_col][i] = np.nan
+
+            #TODO: delete this when confident it's not needed
+            '''
+            print(f"|{label}|")
+            if label == "":
+                raise Exception
+            elif np.isnan(label):
+            #also don't overwrite here, should keep multiple fail reasons
+                self._failed[i] = "Missing initial label"
+
+            #missing_labels =self.dataset[self._label_col].astype(float))
+            #missing_labels = np.isnan(self.dataset[self._label_col].astype(float))
+            #new_fail_dict = {x:"Missing initial label" for x in self.dataset[missing_labels].index}
+            #self._failed.update(new_fail_dict)
+            '''
 
         # save the initial labels to the dictionary
         if self._label == "continuous":

descriptor.py
@@ -7,7 +7,18 @@ from rdkit.DataStructs import ConvertToNumpyArray
 
 # TODO add logging for when descriptors are None, dont want to remove any rows here just return Nan/Nones
 
-class MorganDescriptor:
+class Descriptor:
+
+    def to_dict(self):
+        raise NotImplementedError
+
+    def get_descriptors(self, romols):
+        raise NotImplementedError
+
+    def to_string(self):
+        raise NotImplementedError
+
+class MorganDescriptor(Descriptor):
 
     def __init__(self, radius=3, n_bits=2048, count=False, use_chirality=False, use_cached=False):
         self.radius = radius
@@ -54,6 +65,10 @@ class MorganDescriptor:
         fp = np.vstack(fp)
 
         return fp
+
+    def get_string(self):
+
+        return f"morgan_fingerprint_radius_{self.radius}_nbits_{self.n_bits}_count_{self.count}_chiral_{self.use_chirality}"
 
 
 

plate.py
@@ -1,6 +1,13 @@
 import os
 import yaml
 
+def KFoldSplitter(x, y, n_folds = 5):
+
+    from sklearn.model_selection import KFold
+    kf = KFold(n_splits = n_folds)
+    for train_indices, test_indices in kf.split(x):
+        yield (x[train_indices], x[test_indices], y[train_indices], y[test_indices])
+
 
 class Plate:
 
@@ -81,32 +88,109 @@ class Plate:
     def run(self):
 
         import itertools
+        import pandas as pd
+
+        modeling_results = []
         for this_dataset, this_model, this_descriptor_function in itertools.product(self.datasets, self.models, self.descriptor_functions):
+
+            s = pd.Series()
+            s["Dataset"] = this_dataset.name
+            s["Model"] = this_model.get_string()
+            s["Descriptor"] = this_descriptor_function.get_string()
+
 
             descriptor_matrix = this_descriptor_function.get_descriptors(this_dataset.get_dataset()["ROMol"])
             if "binary" not in this_dataset._labels:
                 this_dataset.to_binary()
             labels = this_dataset.get_labels(kind = "binary")
 
+            from qsar_utils import modi
+            s["MODI"] = modi(descriptor_matrix, labels)
 
             from sklearn.model_selection import train_test_split
 
+            results = []
 
-            x_train, x_test, y_train, y_test = train_test_split(descriptor_matrix, labels, test_size = 0.5)
+            for i, indx in enumerate(KFoldSplitter(descriptor_matrix, labels, n_folds = 5)):
+                 
+                s["Fold"] = i
 
-            this_model.fit(x_train, y_train)
-            pred = this_model.predict_probability(x_test)
-            train_pred = this_model.predict_probability(x_train)
+                x_train, x_test, y_train, y_test = indx
 
-            print(y_test)
-            print(pred)
+                this_model.fit(x_train, y_train)
+                pred = this_model.predict_probability(x_test)
+                train_pred = this_model.predict_probability(x_train)
+
+                print(y_test)
+                print(pred)
 
 
+                test_s = s.copy()
+                train_s = s.copy()
+                test_s["target"] = "Test" 
+                train_s["target"] = "Train"
+                test_s["predictions"] = pred
+                train_s["predictions"] = train_pred
 
-            from metrics import get_classification_metrics, auc
-            #stats = get_classification_metrics(y_test, pred)
-            auc = auc(y_test, pred)
-            print(auc)
+                train_s["true labels"] = y_train
+                test_s["true labels"] = y_test
+
+                '''
+                from qsar_utils import apd_screening
+                train_ad = apd_screening(y = x_train, training_data=x_train, threshold=None, norm_func=None)
+                test_ad = apd_screening(y = x_test, training_data=x_train, threshold=None, norm_func=None)
+                train_coverage = sum(train_ad) / len(train_ad)
+                test_coverage = sum(test_ad) / len(test_ad)
+
+                train_s["ad coverage"] = train_coverage 
+                test_s["ad coverage"] = test_coverage 
+                '''
+
+                from metrics import get_classification_metrics, auc
+                train_s["auc"] = auc(y_train, train_pred)
+                test_s["auc"] = auc(y_test, pred)
+
+                for standard_threshold in [0.5, 0.6, 0.7, 0.8, 0.9]:
+
+                    
+                    this_test_s = test_s.copy()
+                    this_test_s["Classification threshold"] = standard_threshold
+                    thresholded = pred >= standard_threshold
+                    stats = get_classification_metrics(y_test, thresholded)
+
+                    for key, val in stats.items():
+                        if key in this_test_s:
+                            raise Exception("Overwriting a statistic?")
+                        this_test_s[key] = val
+
+                    this_train_s = train_s.copy()
+                    this_train_s["Classification threshold"] = standard_threshold
+                    thresholded = train_pred >= standard_threshold
+                    stats = get_classification_metrics(y_train, thresholded)
+
+                    for key, val in stats.items():
+                        if key in this_train_s:
+                            raise Exception("Overwriting a statistic?")
+                        this_train_s[key] = val
+
+                    modeling_results.append(this_train_s)
+                    modeling_results.append(this_test_s)
+
+            modeling_results = pd.DataFrame(modeling_results)
+            modeling_results = modeling_results.sort_values(by = ["target", "Classification threshold", "Fold"])
+            modeling_results.drop(columns = ["predictions", "true labels"]).to_csv("stats.csv")
+            print(modeling_results)
+            exit()
+
+
+            test_df = modeling_results[modeling_results["target"] == "Test"]
+            train_df = modeling_results[modeling_results["target"] == "Train"]
+
+            threshold_plot_2(test_df["true labels"], test_df["predictions"], filename = "test.png")
+            threshold_plot_2(train_df["true labels"], train_df["predictions"], filename = "train.png")
+            result_df = pd.DataFrame(results, columns = ["Metric", "Fold"])
+            print(result_df)
+            exit()
 
             file_string = this_dataset.name + "_" + this_model.name + "_" + this_descriptor_function.name
             print(file_string)
@@ -114,8 +198,8 @@ class Plate:
             print(file_string)
 
             extra_data = f"Dataset: {this_dataset.name}\nDescriptor: {this_descriptor_function.name}\nModel: {this_model.name}"
-            threshold_plot(y_test, pred, file_string + "_test_threshold_figure.png", extra_data + "\nPredict on: Test")
-            threshold_plot(y_train, train_pred, file_string + "_train_threshold_figure.png", extra_data + "\nPredict on: Train")
+            threshold_plot_2(y_test, pred, file_string + "_test_threshold_figure.png", extra_data + "\nPredict on: Test")
+            threshold_plot_2(y_train, train_pred, file_string + "_train_threshold_figure.png", extra_data + "\nPredict on: Train")
 
 def clean_name(name):
 
@@ -124,6 +208,14 @@ def clean_name(name):
     
 
 def threshold_plot(y_true, y_pred, filename, extra_data = None):
+
+    import pandas as pd
+    if type(y_true) == pd.Series:
+        y_true = list(y_true)
+        y_pred = list(y_pred)
+
+    print(y_true)
+
 
     from metrics import get_classification_metrics, auc, ppv, npv, accuracy, balanced_accuracy
 
@@ -156,6 +248,116 @@ def threshold_plot(y_true, y_pred, filename, extra_data = None):
     ax2.plot(thresholds, accuracies, label = "Accuracy")
     ax2.plot(thresholds, balanced_accuracies, label = "Balanced Accuracy")
     ax1.plot(thresholds, nums_active, color = "red", linestyle = "--")
+    ax2.yaxis.label.set_text("Value of Metric")
+    ax1.yaxis.label.set_text("--- Number of predicted actives")
+    ax1.yaxis.label.set_color("red")
+    ax2.set_xlabel("Classification Threshold")
+    ax1.spines['right'].set_color('red')
+    ax1.tick_params(axis = 'y', colors = 'red')
+    plt.title("Validation Stats as a Function of Changing Threshold")
+    ax2.set_xticks(np.arange(0,1.1,0.1))
+    ax2.set_yticks(np.arange(0,1.1,0.1))
+    ax2.set_xlim((0, 1))
+    ax2.set_ylim((0, 1))
+    ax1.set_xlim((0, 1))
+    ax1.set_ylim((0, max(nums_active)))
+    ax2.grid(visible = True, axis = 'both', alpha = 0.5)
+    ax2.legend(bbox_to_anchor=(-0.1, 1), loc='upper right', ncol=1)
+
+    if extra_data:
+        ax2.text(-0.5, 0.5, extra_data, fontsize = 8)
+    plt.savefig(filename, bbox_inches = "tight", dpi = 300)
+
+
+def threshold_plot_2(y_true, y_pred, filename, extra_data = None):
+
+    import pandas as pd
+    if type(y_true) == pd.Series:
+        y_true = list(y_true)
+        y_pred = list(y_pred)
+
+    print(y_true)
+
+
+    from metrics import get_classification_metrics, auc, ppv, npv, accuracy, balanced_accuracy
+
+    ppvs = []
+    npvs = []
+    accuracies = []
+    balanced_accuracies = []
+    nums_active = []
+
+    import numpy as np
+
+    thresholds = np.arange(0,1,0.01)
+
+    all_ppvs = []
+    all_npvs = []
+    all_accuracies = []
+    all_balanced_accuracies = []
+    all_nums_active = []
+
+    for i in range(len(y_pred)):
+
+        ppvs = []
+        npvs = []
+        accuracies = []
+        balanced_accuracies = []
+        nums_active = []
+
+        this_y_pred = y_pred[i]
+        this_y_true = y_true[i]
+
+        for threshold in thresholds:
+
+            thresholded = this_y_pred > threshold
+
+            ppvs.append(ppv(this_y_true, thresholded))
+            npvs.append(npv(this_y_true, thresholded))
+            accuracies.append(accuracy(this_y_true, thresholded))
+            balanced_accuracies.append(balanced_accuracy(this_y_true, thresholded))
+            nums_active.append(sum(thresholded))
+
+        all_ppvs.append(ppvs)
+        all_npvs.append(npvs)
+        all_accuracies.append(accuracies)
+        all_balanced_accuracies.append(balanced_accuracies)
+        all_nums_active.append(nums_active)
+
+
+    ppv_mean = np.mean(np.array(all_ppvs), axis = 0)
+    ppv_std = np.std(np.array(all_ppvs), axis = 0)
+
+    npv_mean = np.mean(np.array(all_npvs), axis = 0)
+    npv_std = np.std(np.array(all_npvs), axis = 0)
+
+    accuracy_mean = np.mean(np.array(all_accuracies), axis = 0)
+    accuracy_std = np.std(np.array(all_accuracies), axis = 0)
+
+    balanced_accuracy_mean = np.mean(np.array(all_balanced_accuracies), axis = 0)
+    balanced_accuracy_std = np.std(np.array(all_balanced_accuracies), axis = 0)
+
+    num_active_mean = np.mean(np.array(all_nums_active), axis = 0)
+    num_active_std = np.std(np.array(all_nums_active), axis = 0)
+
+    import matplotlib.pyplot as plt
+
+    plt.figure()
+    fig, ax2 = plt.subplots()
+    ax1 = ax2.twinx()
+    ax2.plot(thresholds, ppv_mean, label = "PPV")
+    ax2.fill_between(thresholds, ppv_mean - ppv_std, ppv_mean + ppv_std, alpha = 0.3)
+    ax2.plot(thresholds, npv_mean, label = "NPV")
+    ax2.fill_between(thresholds, npv_mean - npv_std, npv_mean + npv_std, alpha = 0.3)
+    ax2.plot(thresholds, accuracy_mean, label = "Accuracy")
+    ax2.fill_between(thresholds, accuracy_mean - accuracy_std, accuracy_mean + accuracy_std, alpha = 0.3)
+    ax2.plot(thresholds, balanced_accuracy_mean, label = "Balanced Accuracy")
+    ax2.fill_between(thresholds, balanced_accuracy_mean - balanced_accuracy_std, balanced_accuracy_mean + balanced_accuracy_std, alpha = 0.3)
+    ax1.plot(thresholds, num_active_mean, color = "red", linestyle = "--")
+    print(num_active_mean)
+    print(num_active_std)
+    #ax2.fill_between(thresholds, num_active_mean - num_active_std, num_active_mean + num_active_std, alpha = 0.3, color = "red")
+    ax2.fill_between(thresholds, num_active_mean - num_active_std, num_active_mean + num_active_std, alpha = 1, color = "red")
     ax2.yaxis.label.set_text("Value of Metric")
     ax1.yaxis.label.set_text("--- Number of predicted actives")
     ax1.yaxis.label.set_color("red")

qsar_utils.py
@@ -31,6 +31,7 @@ def nearest_neighbors(reference, query, k=1, self_query=False, return_distance=F
             The distances to the nearest neighbors.
     """
 
+
     # use scipy's kdtree for extra speed qsar go brrrr
     tree = sp.KDTree(reference)
 
@@ -48,6 +49,7 @@ def nearest_neighbors(reference, query, k=1, self_query=False, return_distance=F
 
 
 def modi(data, labels):
+
     # get all the classes present in the dataset
     classes = np.unique(labels)
     k = classes.shape[0]
@@ -61,8 +63,7 @@ def modi(data, labels):
     for c in classes:
         c_arr = np.where(labels == c)[0]
         c_labels = labels[c_arr]
-        c_nn_labels = nn_labels[c_arr]
-
+        c_nn_labels = nn_labels[c_arr].transpose()
         modi_value += np.sum(c_labels == c_nn_labels) / c_arr.shape[0]
 
     return (k ** -1) * modi_value