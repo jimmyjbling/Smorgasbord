2dd153d5781b96e256807c45b7e63df5c25b5d9a
James Wellnitz <wellnitz.james@gmail.com>
2022-08-02 14:39:44 -0500

    added some quick sampling functions

--- sampling.py
+++ sampling.py
@@ -1,18 +1,34 @@
 class Sampler:
-    def __init__(self):
+    def __init__(self, cache=False, random_state=None):
+        self._random_state = random_state
+        self._cache = cache
+
+    def undersample(self, X, y):
+        from imblearn.under_sampling import RandomUnderSampler
+
+        ros = RandomUnderSampler(random_state=self._random_state)
+        X_res, y_res = ros.fit_resample(X, y)
+
+        return X_res, y_res
+
+    def oversample(self, X, y):
+        from imblearn.over_sampling import RandomOverSampler
+
+        ros = RandomOverSampler(random_state=self._random_state)
+        X_res, y_res = ros.fit_resample(X, y)
+
+        return X_res, y_res
+
+    def remove_most_similar(self, X, y):
         raise NotImplementedError
 
-    def undersample(self):
+    def remove_least_similar(self, X, y):
         raise NotImplementedError
 
-    def oversample(self):
-        raise NotImplementedError
+    def smote(self, X, y):
+        from imblearn.over_sampling import SMOTE
 
-    def remove_most_similar(self):
-        raise NotImplementedError
+        sm = SMOTE(random_state=self._random_state)
+        X_res, y_res = sm.fit_resample(X, y)
 
-    def remove_least_similar(self):
-        raise NotImplementedError
-
-    def smote(self):
-        raise NotImplementedError
+        return X_res, y_res
