061ae7f0449c29dad30ffbcc72011e58e95790ad
James Wellnitz <wellnitz.james@gmail.com>
2022-08-02 12:01:12 -0500

    added procedure interface and classes

---
+++ procedure.py
@@ -0,0 +1,21 @@
+class BaseProcedure:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+
+class Screen(BaseProcedure):
+    def __init__(self):
+        super().__init__()
+
+
+class Train(BaseProcedure):
+    def __init__(self):
+        super().__init__()
+
+
+class CrossValidate(BaseProcedure):
+    def __init__(self):
+        super().__init__()
