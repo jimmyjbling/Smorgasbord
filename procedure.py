cbf63d0606f3bd374d5cbfb09d51a93bc95d0f34
James Wellnitz <wellnitz.james@gmail.com>
2022-08-02 15:06:02 -0500

    added the screening function

--- procedure.py
+++ procedure.py
@@ -1,12 +1,15 @@
 class Procedure:
-    def __init__(self):
+    def __init__(self, random_state=None):
+        self._random_state = random_state
+
+    def screen(self, model, screening_X, predict_prob=False):
+        if predict_prob:
+            return model.predict_proba(screening_X)
+        else:
+            return model.predict(screening_X)
+
+    def train(self, model, training_dataset):
         pass
 
-    def screen(self, model, screening_df):
-        pass
-
-    def train(self, model, training_df):
-        pass
-
-    def cross_validate(self, model, training_df, cv=None):
+    def cross_validate(self, model, training_dataset, cv=None):
         pass
